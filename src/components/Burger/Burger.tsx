b10cbb9becae9f3a68de7df70595f224ef911d1a
Maksym Karamanov <maximanbw@gmail.com>
2019-09-25 17:09:27 +0300

    Add state & outputt burger ingredients dynamically

--- src/components/Burger/Burger.tsx
+++ src/components/Burger/Burger.tsx
@@ -3,15 +3,22 @@ import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 interface Props {
-
+  ingredients: {
+    [ingredient: string]: number
+  }
 }
 
 const burger: React.FC<Props> = (props) => {
+  const transformedIngredients = Object.keys(props.ingredients)
+    .map((igKey: string) => {
+      return [...Array(props.ingredients[igKey])].map(( i => {
+        return <BurgerIngredient key={igKey+i} type={igKey} />
+      }))
+    })
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type='bread-top' />
-      <BurgerIngredient type='cheese' />
-      <BurgerIngredient type='meat' />
+      {transformedIngredients}
       <BurgerIngredient type='bread-bottom' />
     </div>
   )
--- src/containers/BurgerBuilder/BurgerBuilder.tsx
+++ src/containers/BurgerBuilder/BurgerBuilder.tsx
@@ -6,16 +6,25 @@ interface Props {
 
 }
 interface State {
-
+  ingredients: {
+    [ingredient: string]: number
+  }
 }
 
 export default class BurgerBuilder extends Component<Props, State> {
-  state = {}
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      cheese: 2,
+      meat: 2
+    }
+  }
 
   render() {
     return (
       <Aux>
-        <Burger />
+        <Burger ingredients={this.state.ingredients}/>
         <div>Build Controls</div>
       </Aux>
     )
